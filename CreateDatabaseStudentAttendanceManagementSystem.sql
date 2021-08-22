c6f3b5a298943791dcf7034a2a024924142b0355
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-08-22 16:33:43 +0530

    creation sql queries implemented

--- CreateDatabaseStudentAttendanceManagementSystem.sql
+++ CreateDatabaseStudentAttendanceManagementSystem.sql
@@ -1,1 +1,2 @@
-CREATE DATABASE `studentattendancemanagementsystem` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `studentattendancemanagementsystem`;
+USE studentattendancemanagementsystem;
