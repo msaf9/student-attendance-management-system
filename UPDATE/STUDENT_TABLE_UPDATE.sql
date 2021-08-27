40e50c90a66a020a5f710238b1b344493c1d3002
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-08-28 00:05:17 +0530

    wrong sql query

--- UPDATE/STUDENT_TABLE_UPDATE.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO STUDENT VALUES(150030590, 'Sahil Afrid', 'sahil.afrid@kluniversity.in', 2, 2, 150031);
-INSERT INTO STUDENT VALUES(150030659, 'Sujith Mohan', 'sujith.mohan@kluniversity.in', 2, 1, 150024);
-INSERT INTO STUDENT VALUES(150030290, 'Sai Aravind', 'sai.aravind@kluniversity.in', 2, 3, 150022);
