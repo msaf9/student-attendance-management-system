6736eb2f771b9b0dfd8e49c34942e7981f194a0d
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-08-28 00:41:49 +0530

    changes made to reduce the id size

---
+++ UPDATE/STUDENT_TABLE_UPDATE.sql
@@ -0,0 +1,3 @@
+INSERT INTO STUDENT VALUES(590, 'Sahil Afrid', 'sahil.afrid@kluniversity.in', 2, 2, 150031);
+INSERT INTO STUDENT VALUES(659, 'Sujith Mohan', 'sujith.mohan@kluniversity.in', 2, 1, 150024);
+INSERT INTO STUDENT VALUES(290, 'Sai Aravind', 'sai.aravind@kluniversity.in', 2, 3, 150022);
